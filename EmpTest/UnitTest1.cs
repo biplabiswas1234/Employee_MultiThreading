cd9fcc96f1da9a1734ba0809bb8ccd9acb8ae973
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-25 16:09:45 +0530

    [Biplab] Add: Calculating time for adding employee details

---
+++ EmpTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+using Employee_MultiThreading;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace EmpTest
+{
+    public class Tests
+    {
+        List<EmployeeDetails> employeeDetails;
+        EmployeePayrollOperation emppayroll;
+        [SetUp]
+        public void Setup()
+        {
+            employeeDetails = new List<EmployeeDetails>();
+            emppayroll = new EmployeePayrollOperation();
+        }
+
+        [Test]
+        public void CheckingTime_Required_For_Inserting_Details()
+        {
+            employeeDetails.Add(new EmployeeDetails(ID: 1, Name: "Biplab", StartDate: Convert.ToDateTime("2022-03-01"), Gender: "M", PhoneNumber: 9987856958, Address: "Pune", Department: "IT", BasicPay: 70000, Deduction: 100, TaxablePay: 100, IncomeTax: 100, NetPay: 69700));
+            employeeDetails.Add(new EmployeeDetails(ID: 2, Name: "Binita", StartDate: Convert.ToDateTime("2022-01-09"), Gender: "F", PhoneNumber: 9920331100, Address: "Pune", Department: "IT", BasicPay: 70000, Deduction: 100, TaxablePay: 100, IncomeTax: 100, NetPay: 69700));
+            employeeDetails.Add(new EmployeeDetails(ID: 3, Name: "Kenny", StartDate: Convert.ToDateTime("2022-06-29"), Gender: "F", PhoneNumber: 9920331100, Address: "Pune", Department: "IT", BasicPay: 70000, Deduction: 100, TaxablePay: 100, IncomeTax: 100, NetPay: 69700));
+            employeeDetails.Add(new EmployeeDetails(ID: 4, Name: "Hades", StartDate: Convert.ToDateTime("2022-05-19"), Gender: "M", PhoneNumber: 9920331100, Address: "Pune", Department: "IT", BasicPay: 70000, Deduction: 100, TaxablePay: 100, IncomeTax: 100, NetPay: 69700));
+            employeeDetails.Add(new EmployeeDetails(ID: 5, Name: "Gigi", StartDate: Convert.ToDateTime("2022-03-02"), Gender: "F", PhoneNumber: 9920331100, Address: "Pune", Department: "IT", BasicPay: 70000, Deduction: 100, TaxablePay: 100, IncomeTax: 100, NetPay: 69700));
+
+            DateTime StartDateTime = DateTime.Now;
+            emppayroll.AddEmployee(employeeDetails);
+            DateTime StopDateTimes = DateTime.Now;
+            Console.WriteLine("Duration without threads: " + (StopDateTimes - StartDateTime));
+        }
+    }
+}
---
+++ Employee_MultiThreading/EmployeePayrollOperation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_MultiThreading
+{
+    public class EmployeePayrollOperation
+    {
+        public List<EmployeeDetails> employeeDatalist = new List<EmployeeDetails>();
+        public void AddEmployee(List<EmployeeDetails> employeeDatalist)
+        {
+            employeeDatalist.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee being added: " + employeeData.Name);
+                this.AddEmployeePayroll(employeeData);
+                Console.WriteLine("Employee added: " + employeeData.Name);
+            }
+            );
+            Console.WriteLine(this.employeeDatalist.ToString());
+        }
+        public void AddEmployeePayroll(EmployeeDetails employee)
+        {
+            employeeDatalist.Add(employee);
+        }
+    }
+}
